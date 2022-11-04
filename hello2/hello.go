a7524d1bbac5461e1422c7d8e8465383e748aaff
SeungWookHan <hswook12@me.com>
2022-11-04 17:15:44 +0900

    Fix greeting code as return a random string

--- hello2/hello.go
+++ hello2/hello.go
@@ -11,11 +11,11 @@ func main() {
 	log.SetPrefix("greetings: ")
 	log.SetFlags(0)
 
-	message, err := greetings.Hello("")
+	// message, err := greetings.Hello("")
+	message, err := greetings.Hello("Gladys")
 
 	if err != nil {
 		log.Fatal(err)
 	}
-	// message := greetings.Hello("Gladys")
 	fmt.Println(message)
 }
